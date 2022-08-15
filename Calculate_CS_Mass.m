a58d65613e6d785f7ef1ad6919b257114e4852b0
MarkVygoder <mvygoder@uwm.edu>
2022-08-14 22:55:14 -0500

    3D Plots
    Added file with a 3D plot for the chopper stage as an example. Will work to figure out others.

--- Calculate_CS_Mass.m
+++ Calculate_CS_Mass.m
@@ -10,20 +10,20 @@ function [CS_Mass] = Calculate_CS_Mass(F, Vin, Vout, Pout, Available_Modules, Re
 
     CSM_mass_coefficient = 0.645; % mass coefficient
 
-    CSM_efficiency_factor = exp(0.01./(1-CSE))/1.5169;
+    CSM_efficiency_factor = exp(0.01./(1-CSE))./1.5169;
 
-    CSM_redundancy_factor = CSAM/CSRM;
+    CSM_redundancy_factor = CSAM./CSRM;
 
     CSM_power_level_multiplier = CSPo;
 
-    CSM_power_level_factor = (CSPo/CSRM)^(-0.065);
+    CSM_power_level_factor = (CSPo./CSRM).^(-0.065);
 
-    CSM_voltage_level_factors = (CSVi/(CSVi-2))^7 * exp(CSVi/40000);
+    CSM_voltage_level_factors = (CSVi./(CSVi-2)).^7 * exp(CSVi./40000);
 
-    CSM_frequency_factors = (20/CSF)^0.45 * exp(CSPo^0.1 * CSF/200);
+    CSM_frequency_factors = (20./CSF).^0.45 * exp(CSPo.^0.1 * CSF./200);
 
-      CS_Mass = CSM_mass_coefficient * CSM_efficiency_factor * CSM_redundancy_factor ...
-    * CSM_power_level_multiplier * CSM_power_level_factor * CSM_voltage_level_factors ...
-    * CSM_frequency_factors;
+      CS_Mass = CSM_mass_coefficient .* CSM_efficiency_factor .* CSM_redundancy_factor ...
+    .* CSM_power_level_multiplier .* CSM_power_level_factor .* CSM_voltage_level_factors ...
+    .* CSM_frequency_factors;
       
 end
